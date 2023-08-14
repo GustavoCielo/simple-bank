40b8bd648d11ba1b841a1d2a97d159c99e145e3a
GustavoCielo <gh.cielo@hotmail.com>
2023-08-14 14:54:19 -0300

    email

---
+++ mail/sender_test.go
@@ -0,0 +1,26 @@
+package mail
+
+// import (
+// 	"testing"
+
+// 	"github.com/GustavoCielo/simple-bank/util"
+// 	"github.com/stretchr/testify/require"
+// )
+
+// func TestSendEmailWithGmail(t *testing.T) {
+// 	config, err := util.LoadConfig("..")
+// 	require.NoError(t, err)
+
+// 	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+// 	subject := "A test email"
+// 	content := `
+// 	<h1>Hello wordl</h1>
+// 	<p>This is a test email</p>
+// 	`
+// 	to := []string{"test@hotmail.com"}
+// 	attachFiles := []string{"../README.md"}
+
+// 	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+// 	require.NoError(t, err)
+// }
