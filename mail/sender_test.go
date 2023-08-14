1642c02b225e6961e143deb329e165f928923808
GustavoCielo <gh.cielo@hotmail.com>
2023-08-14 15:05:02 -0300

    short and skip

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -1,26 +1,29 @@
 package mail
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/GustavoCielo/simple-bank/util"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/GustavoCielo/simple-bank/util"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestSendEmailWithGmail(t *testing.T) {
-// 	config, err := util.LoadConfig("..")
-// 	require.NoError(t, err)
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
 
-// 	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
 
-// 	subject := "A test email"
-// 	content := `
-// 	<h1>Hello wordl</h1>
-// 	<p>This is a test email</p>
-// 	`
-// 	to := []string{"test@hotmail.com"}
-// 	attachFiles := []string{"../README.md"}
+	subject := "A test email"
+	content := `
+	<h1>Hello wordl</h1>
+	<p>This is a test email</p>
+	`
+	to := []string{"test@hotmail.com"}
+	attachFiles := []string{"../README.md"}
 
-// 	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
-// 	require.NoError(t, err)
-// }
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
